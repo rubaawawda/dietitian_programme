5eba8f8cc860f11741cf54af674fc45ca32eade4
Ruba <ruba@gmail>
2023-01-23 13:35:23 +0200

    select meal

--- src/common/tabs/tabs.common.jsx
+++ src/common/tabs/tabs.common.jsx
@@ -2,62 +2,64 @@ import React, { useState } from "react";
 import "./tabs.css";
 
 function Tabs({ tabs = [], editable = false }) {
-  const [activeTabIndex, setActiveTabIndex] = useState(0);
-  const [allTabs, setAllTabs] = useState(tabs);
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
+    const [allTabs, setAllTabs] = useState(tabs);
 
-  const NewTabButton = (
-    <div className="btn" onClick={() => createNewTab()}>
-      +
-    </div>
-  );
+    const NewTabButton = (
+        <div className="btn" onClick={() => createNewTab()}>
+            +
+        </div>
+    );
 
-  const NewTab = (
-    <div>
-      <label>New Tab</label>
-      <p>This is a new tab.</p>
-    </div>
-  );
+    const NewTab = (
+        <div>
+            <label>New Tab</label>
+            <p>This is a new tab.</p>
+        </div>
+    );
 
-  const createNewTab = () => {
-    const newTabs = allTabs;
-    newTabs.push({ name: "New Tab", content: NewTab });
-    setAllTabs(newTabs);
-    setActiveTabIndex(newTabs.length - 1);
-  };
+    const createNewTab = () => {
+        const newTabs = allTabs;
+        newTabs.push({ name: "New Tab", content: NewTab });
+        setAllTabs(newTabs);
+        setActiveTabIndex(newTabs.length - 1);
+    };
 
-  const activateTab = (index) => {
-    setActiveTabIndex(index);
-  };
+    const activateTab = (tab,index) => {
+        setActiveTabIndex(index);
+        {console.log(tab.name)}
+    };
 
-  return (
-    <div className="TabView">
-     
-      <div className="body">
-        {allTabs.length === 0 ? (
-          <div className="tabs">
-            <div>No Tabs</div>
-            {editable ? NewTabButton : null}
-          </div>
-        ) : (
-          <div>
-            <div className="tabs">
-              {allTabs.map((tab, index) => (
-                <label
-                  key={index}
-                  className={index === activeTabIndex ? "active-tab" : "tab"}
-                  onClick={() => activateTab(index)}
-                >
-                  {tab.name}
-                </label>
-              ))}
-              {editable ? NewTabButton : null}
+    return (
+        <div className="TabView">
+
+            <div className="body">
+                {allTabs.length === 0 ? (
+                    <div className="tabs">
+                        <div>No Tabs</div>
+                        {editable ? NewTabButton : null}
+                    </div>
+                ) : (
+                    <div>
+                        <div className="tabs">
+                            {allTabs.map((tab, index) => (
+                                <label
+                                    key={index}
+                                    className={index === activeTabIndex ? "active-tab" : "tab"}
+                                    onClick={() => activateTab(tab,index)}
+                                >
+                                    {tab.name}
+                                   
+                                </label>
+                            ))}
+                            {editable ? NewTabButton : null}
+                        </div>
+                        <div className="content">{allTabs[activeTabIndex].content}</div>
+                    </div>
+                )}
             </div>
-            <div className="content">{allTabs[activeTabIndex].content}</div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Tabs;
--- src/pages/new-diet-program/popup.jsx
+++ src/pages/new-diet-program/popup.jsx
@@ -3,38 +3,82 @@ import './popup.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import Select from "../../common/select/select";
-//import { useState } from 'react';
+import { useState } from 'react';
+import Input from '../../common/input/input.common';
 //import Select from reactSelect
 //import Select from "react-select";
+import { useEffect } from 'react';
 
 
 
 const MealPopup = (props) =>{
     const items = JSON.parse(localStorage.getItem('listItem'));
- 
-  
+    const [selectList, setSelectList]=useState('')
+    const [amountValue, setAmountValue]= useState('');
+      const [selected, setSelected] = useState(''); 
+      const value = selected && items[selected];
+
+      useEffect(() => {
+        const items = JSON.parse(localStorage.getItem('selectItem')|| '[]') ;
+        setSelectList(items)
+      }, []);
+
+      const handleAddSubmit = (event) => {
+        
+        event.preventDefault();
+    
+        const Item = {
+          
+          name: " Ahmad",
+          saturday: [{name:"rice", calories:"99"}],
+          saturday: [{name:"bread", calories:"9"}],
+        };
+        
+        const menuItems = [...selectList, Item];
+        setSelectList(menuItems);
+        localStorage.setItem("selectItem", JSON.stringify(menuItems));
+        
+        
+      };
+
     return (props.trigger) ? (
 
         <div className="popup">
-          <form className="inner-popup">
+          <form className="inner-popup" onSubmit={handleAddSubmit}>
             <FontAwesomeIcon className="close-btn" onClick={() => props.setTrigger(false)} icon={faXmark} />
             {props.children}
             <h3>Add food meal</h3>
             <div className="box">
                 
-            <Select name="city" placeholder="city" >
-                        
-                       
-                            {
-                                items.map(item=>{
-                                    return (
-                                    <option key={item} value={item}>{item.name}</option>
-                                    )
-                                })
-                            }
-                        
-                            
-                     </Select>
+            <Select 
+      onChange={(e) => setSelected(e.target.value)}
+      value={selected}>
+    
+      <option>Selecte a Meal...</option>
+      {items.map((m, index) => {
+        return <option 
+          key={m.id}
+          value={index}
+        >
+         {m.name}
+        </option>
+      })};
+    </Select>
+
+    <Input
+            type="numper"
+            name="name"
+            required="required"
+            placeholder="Amount"
+            onChange={(e)=>{setAmountValue(e.target.value)}}
+        
+          />
+    
+    <div >
+      <img src={value.image} alt="img"/>
+      
+      <div>Total calories: {value.calories/value.amount*amountValue}</div>
+    </div>
                     
                       
             </div>
